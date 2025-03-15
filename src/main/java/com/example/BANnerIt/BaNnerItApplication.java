04dffe55737df333ccae95af833fed6ecbaa7381
DaYeong <day0_ng@naver.com>
2025-03-16 00:21:59 +0900

    chore: Initial commit

---
+++ src/main/java/com/example/BANnerIt/BaNnerItApplication.java
@@ -0,0 +1,13 @@
+package com.example.BANnerIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaNnerItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaNnerItApplication.class, args);
+	}
+
+}
