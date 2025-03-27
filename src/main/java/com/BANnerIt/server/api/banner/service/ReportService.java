7fd8a02729a51c9485d11b7baccf77918f701a8b
JangYujin <yujin24300@naver.com>
2025-03-28 01:02:16 +0900

    feat: Banner & Report 서비스 기능 추가
    -현수막 신고 기록 저장 및 조회
    -현수막 정보 저장 및 수정
    -Member 엔티티에 Banner와 연관 관계 추가 (OneToMany)

---
+++ src/main/java/com/BANnerIt/server/api/banner/service/ReportService.java
@@ -0,0 +1,106 @@
+package com.BANnerIt.server.api.banner.service;
+
+import com.BANnerIt.server.api.banner.domain.Banner;
+import com.BANnerIt.server.api.banner.domain.Report;
+import com.BANnerIt.server.api.banner.domain.ReportStatus;
+import com.BANnerIt.server.api.banner.dto.banner.BannerDetailsWithIdDto;
+import com.BANnerIt.server.api.banner.dto.report.*;
+import com.BANnerIt.server.api.banner.repository.ReportRepository;
+import com.BANnerIt.server.api.user.Member;
+import com.BANnerIt.server.api.user.repository.MemberRepository;
+import com.BANnerIt.server.global.auth.JwtTokenUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReportService {
+    private final ReportRepository reportRepository;
+    private final MemberRepository memberRepository;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    //사진 저장 아직 안함
+    /*현수막 신고 저장*/
+    @Transactional
+    public Long saveReport(String token, SaveReportRequest request){
+        ReportDto reportLog = request.report();
+        LocationDto location = reportLog.location();
+        AddressDto address = reportLog.address();
+
+        Long userId = jwtTokenUtil.extractUserId(token);
+        Member member = memberRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        Report report = Report.builder()
+                .content(reportLog.content())
+                .latitude(location.latitude())
+                .longitude(location.longitude())
+                .address1(address.address1())
+                .address2(address.address2())
+                .address3(address.address3())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .status(ReportStatus.RECEIVED)
+                .createdBy(member)
+                .build();
+
+        reportRepository.save(report);
+        return report.getReportId();
+    }
+
+    /*사용자의 현수막 신고기록 조회*/
+    public List<ReportLogDto> getUserReportLogs(String token){
+        Long userId = jwtTokenUtil.extractUserId(token);
+        Member member = memberRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        List<ReportLogDto> reportLogs = new ArrayList<>();
+
+        for(Report r : member.getCreatedReports()){
+            ReportLogDto report = getReportLogDto(r);
+
+            reportLogs.add(report);
+        }
+
+        return reportLogs;
+    }
+
+    /*전체 현수막 신고기록 조회*/
+    public List<ReportLogDto> getAllReportLogs(){
+        List<Report> reports = reportRepository.findAll(); // 전체 신고 기록 조회
+        List<ReportLogDto> reportLogs = new ArrayList<>();
+
+        for (Report r : reports) {
+            ReportLogDto report = getReportLogDto(r); // Report 엔티티 -> ReportLogDto로 변환
+            reportLogs.add(report);
+        }
+
+        return reportLogs;
+    }
+
+
+    // 신고 기록을 ReportLogDto로 변환
+    private static ReportLogDto getReportLogDto(Report r) {
+        LocationDto location = new LocationDto(r.getLatitude(), r.getLongitude());
+        List<BannerDetailsWithIdDto> banners = convertBannersToDto(r.getBanners());
+
+        return new ReportLogDto(r.getReportId(), r.getCreatedAt(),
+                r.getStatus(), location, r.getContent(), banners);
+    }
+
+    // 배너 리스트를 DTO로 변환
+    private static List<BannerDetailsWithIdDto> convertBannersToDto(List<Banner> banners) {
+        List<BannerDetailsWithIdDto> bannerDtos = new ArrayList<>();
+        for (Banner b : banners) {
+            BannerDetailsWithIdDto banner = new BannerDetailsWithIdDto(b.getBannerId(),
+                    b.getStatus(), b.getCompanyName(), b.getPhoneNumber());
+            bannerDtos.add(banner);
+        }
+        return bannerDtos;
+    }
+}
