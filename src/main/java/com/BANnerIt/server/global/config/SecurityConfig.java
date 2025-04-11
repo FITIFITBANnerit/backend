74bac0586792283a0f96a51106a26ee5454caa78
DaYeong <day0_ng@naver.com>
2025-04-11 22:47:34 +0900

    fix: CORS 설정 변경
    - 회원정보 수정 요청시 요청 안오는 문제

--- src/main/java/com/BANnerIt/server/global/config/SecurityConfig.java
+++ src/main/java/com/BANnerIt/server/global/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(customAuthenticationEntryPoint))
