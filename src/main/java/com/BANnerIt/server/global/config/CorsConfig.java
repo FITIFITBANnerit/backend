ac866f2c8edb1dff3338a7a12fe2f5f465ea4e0a
DaYeong <day0_ng@naver.com>
2025-04-11 23:55:35 +0900

    fix: CORS 설정 변경
    - 회원정보 수정 요청시 요청 안오는 문제

--- src/main/java/com/BANnerIt/server/global/config/CorsConfig.java
+++ src/main/java/com/BANnerIt/server/global/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("https://bannerit.co.kr", "https://www.bannerit.co.kr")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
